c00f9e86680f7d6c5aeee2213219f694ee9f6268
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-04-30 02:46:07 +0300

    DataStorage<> generic implemented

---
+++ src/DataLayer/DataStorage.java
@@ -0,0 +1,16 @@
+package DataLayer;
+
+import Core.Event;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+//for now mixing what to store and how to do it, maybe split into separate storage and transfer entities
+public interface DataStorage<TStorage, TEntity> {
+    public TEntity deSerializeEvent(TStorage input);
+    public TStorage serializeEvent(TEntity input);
+
+    public String write(Stream<Event> stream) throws IOException;
+    public String write(List<Event> events) throws IOException;
+}
