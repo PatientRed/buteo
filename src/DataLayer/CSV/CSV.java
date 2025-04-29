16be33ffa2f4b6b82703e42efb2010018313e911
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-04-30 02:47:52 +0300

    CSV storage implemented

---
+++ src/DataLayer/CSV/CSV.java
@@ -0,0 +1,84 @@
+package DataLayer.CSV;
+
+import Core.Event;
+import DataLayer.EventsDataStorage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CSV implements EventsDataStorage<String> {
+    private final String HEADER = "event_id,client_id,visited";
+    private final Path PATH;
+    private final String DELIMITER;
+    private final String LINE_SEPARATOR = System.lineSeparator();
+
+    @Override
+    public String write(Stream<Event> stream) throws IOException {
+        try (var bufferedFile = Files.newBufferedWriter(PATH,
+                new StandardOpenOption[]{StandardOpenOption.CREATE,
+                        StandardOpenOption.TRUNCATE_EXISTING,
+                        StandardOpenOption.WRITE})) {
+            bufferedFile.write(HEADER + LINE_SEPARATOR);
+
+            stream.forEach(ev -> {
+                try {
+                    bufferedFile.write(serializeEvent(ev) + LINE_SEPARATOR);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        return PATH.toString();
+    }
+
+    @Override
+    public String write(List<Event> events) throws IOException {
+        try (var bufferedFile = Files.newBufferedWriter(PATH,
+                new StandardOpenOption[]{StandardOpenOption.CREATE,
+                        StandardOpenOption.TRUNCATE_EXISTING,
+                        StandardOpenOption.WRITE})) {
+            bufferedFile.write(HEADER + LINE_SEPARATOR);
+
+            events.forEach(ev -> {
+                try {
+                    bufferedFile.write(serializeEvent(ev) + LINE_SEPARATOR);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        return PATH.toString();
+    }
+
+    @Override
+    public Event deSerializeEvent(String line) throws NumberFormatException, SecurityException {
+        var temp = line.split(DELIMITER);
+        return new Event(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]), Boolean.getBoolean(temp[2]));
+    }
+
+    @Override
+    public String serializeEvent(Event input) {
+        return Long.toString(input.eventId()) + DELIMITER + Long.toString(input.clientId())
+                + DELIMITER + (input.visited() ? "true" : "false");
+    }
+
+    @Deprecated
+    public String serializeNewEventRaw(int eventId, int clientId) {
+        return Long.toString(eventId) + DELIMITER + Long.toString(clientId) + DELIMITER + "false";
+    }
+
+    public CSV(String path, char delimiter) {
+        this.PATH = Path.of(path);
+        this.DELIMITER = Character.toString(delimiter);
+    }
+
+    public CSV(String path) {
+        this(path, ',');
+    }
+}
