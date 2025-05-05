408cb9dba2a0fbf57fae870129fa9a8461ee6fc1
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-05-05 16:46:02 +0300

    main() > light generation & two processing examples added

--- src/buteo/Main.java
+++ src/buteo/Main.java
@@ -14,7 +14,15 @@ public class Main {
 //
 //        System.out.println((double) (t21 - t11) / 1_000_000_000);
 
+        var t12 = System.nanoTime();
+        System.out.println(temp.Emulate(100, 100, 1));
+        var t22 = System.nanoTime();
 
+        System.out.println((double) (t22 - t12) / 1_000_000_000);
+
+        System.out.println(temp.Emulate(100, 5, 2));
+
+        System.out.println(temp.Emulate(95, 95, 2));
 
         System.out.println("No hello.");
     }
