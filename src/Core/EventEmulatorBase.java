bc07d84240ddfac51bde3954545f1a0d264b58c7
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-05-05 16:43:07 +0300

    EEB.takeFiltered() implemented

--- src/Core/EventEmulatorBase.java
+++ src/Core/EventEmulatorBase.java
@@ -1,9 +1,14 @@
 package Core;
 
 import DataLayer.EventsDataStorage;
-import jdk.jshell.spi.ExecutionControl;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class EventEmulatorBase<TStorage> {
@@ -40,6 +45,36 @@ public class EventEmulatorBase<TStorage> {
 
         return Path.of(old.substring(0, lastDotIndex) + "_new" + old.substring(lastDotIndex));
     }
+
+    private static Stream<Event> takeFiltered(int events, int clients, Stream<Event> eventStream) throws RuntimeException {
+        var eventIds = eventStream.filter(ev -> !ev.visited())
+                .collect(Collectors.groupingBy(ev -> ev.eventId()));
+
+        if (eventIds.size() < events)
+            throw new RuntimeException("Not enough events provided.");
+
+        var selected = eventIds.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, kvEntry -> kvEntry.getValue().size()))
+                .entrySet().stream()
+                .filter(kvEntry -> kvEntry.getValue() >= clients)
+                .limit(events)
+                .mapToLong(kvEntry -> kvEntry.getKey())
+                .toArray();
+
+        if (selected.length < events)
+            throw new RuntimeException("Not enough clients provided.");
+
+        var result = new ArrayList<Event>(events * clients);
+
+        for (int i = 0; i < clients; i++) {
+            for (var evId : selected) {
+                result.add(eventIds.get(evId).get(i));
+            }
+        }
+
+        return result.stream();
+    }
+
     private static Stream<Event> visitEvents(Stream<Event> original, Set<Event> visited) {
         return original.map(ev -> visited.contains(ev) ? visitEvent(ev) : ev);
     }
