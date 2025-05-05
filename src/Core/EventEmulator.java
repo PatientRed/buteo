43489f654f631a8db7dd92dad8bc4d62375c9782
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-05-05 16:37:23 +0300

    EE > input/output manipulators implemented

--- src/Core/EventEmulator.java
+++ src/Core/EventEmulator.java
@@ -2,7 +2,25 @@ package Core;
 
 import DataLayer.CSV.CSV;
 
+import java.util.Scanner;
+
 public class EventEmulator extends EventEmulatorBase<String> {
+    @Override
+    protected void askInputRedirect() {
+        System.out.println("Input file?");
+        storage.redirectInput(new Scanner(System.in).nextLine());
+
+        return;
+    }
+
+    @Override
+    protected void askOutputRedirect() {
+        System.out.println("Output file?");
+        storage.redirectOutput(new Scanner(System.in).nextLine());
+
+        return;
+    }
+
     public EventEmulator() {
         storage = new CSV("abc.csv");
     }
