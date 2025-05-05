82ec17e4954cb3d4f7342c8a6646f94c46568271
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-05-05 16:45:30 +0300

    main() > heavy generation example removed

--- src/buteo/Main.java
+++ src/buteo/Main.java
@@ -8,11 +8,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         var temp = new EventEmulator();
 
-        var t1 = System.nanoTime();
-        System.out.println(temp.Emulate(10, 10_000_000, 1));
-        var t2 = System.nanoTime();
+//        var t11 = System.nanoTime();
+//        System.out.println(temp.Emulate(10, 10_000_000, 1));
+//        var t21 = System.nanoTime();
+//
+//        System.out.println((double) (t21 - t11) / 1_000_000_000);
 
-        System.out.println((double) (t2 - t1) / 1_000_000_000);
+
+
         System.out.println("No hello.");
     }
 }
